d1982bddefee8db27c0cd7730514fd8acc10ca2f
Enzxn <92555923+Enzxn@users.noreply.github.com>
2022-08-29 20:02:25 -0300

    Criacao do Interface para Gravar Adicionar no Banco

---
+++ ControleContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,25 @@
+﻿using ControleContatos.Data;
+using ControleContatos.Models;
+
+namespace ControleContatos.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+
+        public ContatoRepositorio(BancoContext bancocontext)
+        {
+            _bancoContext = bancocontext;
+        }
+
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            //gravar no banco de dados
+            _bancoContext.Contatos.Add(contato);
+            _bancoContext.SaveChanges();
+            return contato;
+
+
+        }
+    }
+}
