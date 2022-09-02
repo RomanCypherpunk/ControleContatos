240d052200d0e7a9c66e5fe60ad418e9e0c76c1e
Enzxn <92555923+Enzxn@users.noreply.github.com>
2022-09-01 21:33:44 -0300

    Campos requiridos e obrigatorios

--- ControleContatos/Models/ContatoModel.cs
+++ ControleContatos/Models/ContatoModel.cs
@@ -1,12 +1,19 @@
-﻿namespace ControleContatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleContatos.Models
 {
     public class ContatoModel
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Digite o Nome do Contato")]
 
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Digite o Email do Contato")]
+        [EmailAddress(ErrorMessage = "O Email informado nao é válido.") ]
 
         public string Email { get; set; }
+        [Required(ErrorMessage = "Digite o Celular do Contato")]
+        [Phone(ErrorMessage = "O Celular informado nao é válido.")]
 
         public string Celular { get; set; }
 
